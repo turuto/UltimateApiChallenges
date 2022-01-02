5580056494062ed935bbfb743b258fc4f89df705
Alberto García <turuto@gmail.com>
2022-01-02 08:40:49 +0100

    Weather starter

---
+++ 03_Weather/script.js
@@ -0,0 +1,12 @@
+/**
+ *  STEPS:
+ *
+ * API: https://www.metaweather.com/api/
+ *
+ *  1. Declare a class to GET weather data, and GET the woeid, output received data
+ *  2. Register an event listener and attach it to the GET requests chain from above, adjust UI loading state
+ *  3. Prepare data for the UI in advance and try to use unified structure before outputting to the template
+ *  4. Divide classes per function to have a more clean code approach and separation on concerns
+ *  5. Add error/loading states and cover edge use cases
+ *
+ */
